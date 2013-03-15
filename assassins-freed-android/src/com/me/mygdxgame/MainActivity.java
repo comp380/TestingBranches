611ac7b9fe07093319dc82b0cc672666d96adb9d
Ricky Sidhu <seeingsaturn@gmail.com>
2013-03-14 18:22:55 -0700

    First Commit

---
+++ assassins-freed-android/src/com/me/mygdxgame/MainActivity.java
@@ -0,0 +1,19 @@
+package com.me.mygdxgame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class MainActivity extends AndroidApplication {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+        cfg.useGL20 = false;
+        
+        initialize(new MyGdxGame(), cfg);
+        /*ricky is making a comment to test eclipse pushing.*/
+    }
+}
